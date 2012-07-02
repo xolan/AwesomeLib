ec5b0f2d107b1e3f85f1e968fab1b5f53421939d
Jonas Svarvaa <jonassvarvaa@gmail.com>
2012-07-02 17:01:40 +0200

    Started on a script evaluator.
    This could be useful for example when scripting the different AI-types
    in a game. It's a WIP, and currently very basic.

---
+++ src/script/FileEvaluator.java
@@ -0,0 +1,73 @@
+package script;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.script.ScriptException;
+
+public class FileEvaluator {
+
+	private String engineName;
+	private StringEvaluator evaluator;
+	private Charset charset;
+	
+	public FileEvaluator() {
+		this.engineName = "JavaScript";
+		this.evaluator = new StringEvaluator(this.engineName);
+		this.setCharset(Charset.defaultCharset());
+	}
+	
+	public FileEvaluator(Charset charset) {
+		this.engineName = "JavaScript";
+		this.evaluator = new StringEvaluator(this.engineName);
+		this.setCharset(charset);
+	}
+	
+	public void expose(Object object, String id) {
+		this.evaluator.expose(object, id);
+	}
+	
+	public void evaluate(Path file) throws ScriptException {
+		file = file.toAbsolutePath();
+		StringBuilder content = new StringBuilder();
+		
+		try {
+			BufferedReader reader = Files.newBufferedReader(file, this.charset);
+			Logger.getLogger(this.getClass().getName()).log(Level.INFO, "Reading file: \"" + file + "\".");
+		    String line = null;
+		    while ((line = reader.readLine()) != null) {
+		        content.append(line);
+		    }
+		    Logger.getLogger(this.getClass().getName()).log(Level.INFO, "File successfully read.");
+		} catch(IOException ioe) {
+			Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, "Failed to read file: \"" + file + "\". Are you sure is exists?");
+			ioe.printStackTrace();
+		}
+	    
+	    evaluator.evaluate(content.toString());
+	}
+	
+	private void setCharset(Charset charset) {
+		this.charset = charset;
+	}
+	
+	public static void main(String[] args) {
+		FileEvaluator fe = new FileEvaluator();
+		int multipleBy = 2;
+		fe.expose(multipleBy, "multipleBy");
+		try {
+			fe.evaluate(Paths.get("./scriptTest.js"));
+			System.out.println(multipleBy);
+		} catch (ScriptException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+}
---
+++ src/script/StringEvaluator.java
@@ -0,0 +1,40 @@
+package script;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
+public class StringEvaluator {
+	
+	private ScriptEngineManager factory;
+	private ScriptEngine engine;
+	
+	public StringEvaluator() {
+		this.factory = new ScriptEngineManager();
+		this.engine = factory.getEngineByName("JavaScript");
+	}
+	
+	public StringEvaluator(ScriptEngine engine) {
+		this.factory = new ScriptEngineManager();
+		this.engine = engine;
+	}
+	
+	public StringEvaluator(String engineName) {
+		this.factory = new ScriptEngineManager();
+		this.engine = factory.getEngineByName(engineName);
+	}
+	
+	public void evaluate(String input) throws ScriptException {
+		this.engine.eval(input);
+	}
+	
+	public void expose(Object object, String id) {
+		this.engine.put(id, object);
+	}
+	
+	public static void main(String[] args) throws ScriptException {
+        StringEvaluator se = new StringEvaluator("JavaScript");
+        se.evaluate("print('Hello, World');");
+	}
+	
+}
